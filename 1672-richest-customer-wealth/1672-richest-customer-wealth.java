b852ad0606a5a4d008008ed1021e658e47846f3a
dhruveel10 <66111178+dhruveel10@users.noreply.github.com>
2022-09-27 19:34:53 -0400

    Time: 0 ms (100.00%), Space: 41.6 MB (94.45%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,8 +1,15 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
 	var maxWealth = 0;
-	for (var customer : accounts)
-		maxWealth = Math.max(maxWealth, Arrays.stream(customer).sum());
+
+	for (var customer : accounts) {
+		var wealth = 0;
+		
+		for (var account : customer)
+			wealth += account;
+			
+		maxWealth = Math.max(maxWealth, wealth);
+	}
 	return maxWealth;
-}
+    }
 }
