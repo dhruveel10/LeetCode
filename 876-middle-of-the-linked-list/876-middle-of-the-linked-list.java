b96ad2f0a88008409606e64b3a792b68b838da1d
dhruveel10 <66111178+dhruveel10@users.noreply.github.com>
2022-09-30 18:36:48 -0400

    Time: 0 ms (100.00%), Space: 41.6 MB (53.75%) - LeetHub

---
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    //https://leetcode.com/problems/middle-of-the-linked-list/discuss/1651600/PythonJavaC++-Simple-Solution-oror-One-Pass-oror-Beginner-Friendly-oror-Detailed-Explanation
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
