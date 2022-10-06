757e579ce7a37503a92cf1393796ca902b0913d5
dhruveel10 <66111178+dhruveel10@users.noreply.github.com>
2022-10-06 10:40:34 -0400

    Time: 4 ms (97.64%), Space: 42.3 MB (87.54%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.java
@@ -0,0 +1,30 @@
+class Solution {
+    //43. Multiply Strings - https://leetcode.com/problems/multiply-strings/discuss/17605/Easiest-JAVA-Solution-with-Graph-Explanation
+    public static String multiply(String num1, String num2) {
+        int n = num1.length();
+        int m = num2.length();
+        int[] pos = new int[m+n];
+
+        for(int i = n-1; i>=0; i--){
+            for(int j = m-1; j>=0; j--){
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i+j, p2 = i+j+1;
+                int sum = mul + pos[p2];
+
+                pos[p1] += sum/10;
+                pos[p2] = sum%10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int p:pos)
+            if(!(sb.length() == 0 && p == 0))
+                sb.append(p);
+
+        if(sb.length() == 0)
+            return "0";
+            
+        return sb.toString();
+
+    }
+}
