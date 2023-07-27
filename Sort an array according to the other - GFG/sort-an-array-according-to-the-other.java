c70ce2370eb57f1bc49b75e426441f4086f7795e
dhruveel10 <66111178+dhruveel10@users.noreply.github.com>
2023-07-27 10:59:22 -0400

    Added solution - LeetHub

---
+++ Sort an array according to the other - GFG/sort-an-array-according-to-the-other.java
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+import java.util.HashMap; 
+import java.util.Map; 
+import java.util.Map.Entry; 
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    // A1[] : the input array-1
+    // N : size of the array A1[]
+    // A2[] : the input array-2
+    // M : size of the array A2[]
+    
+    //Function to sort an array according to the other array.
+    public static int[] sortA1ByA2(int A1[], int N, int A2[], int M)
+    {
+        //Your code here
+        int[] count = new int[1000001];
+        int[] ans = new int[N];
+        int pos = 0;
+        
+        for(int i:A1)
+            count[i]++;
+            
+        for(int i = 0; i < M; i++){
+            int temp = A2[i];
+            
+            while(count[temp] != 0){
+                ans[pos] = temp;
+                count[temp]--;
+                pos++;
+            }
+        }
+        
+        for(int i = 0; i < count.length; i++){
+            
+                while(count[i] != 0){
+                    ans[pos] = i;
+                    count[i]--;
+                    pos++;
+                }
+        }
+        
+        return ans;
+    }
+}
+
+
+
+//{ Driver Code Starts.
+class Main {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    int m=sc.nextInt();
+		    int a1[]=new int[n];
+		    int a2[]=new int[m];
+		    
+		    for(int i=0;i<n;i++)
+		    a1[i]=sc.nextInt();
+		    
+		    for(int i=0;i<m;i++)
+		    a2[i]=sc.nextInt();
+		    Solution ob=new Solution();
+		    a1 = ob.sortA1ByA2(a1,n,a2,m);
+		    for(int x:a1)
+		    System.out.print(x+" ");
+		    
+		    System.out.println();
+		}
+	}
+	
+
+}
+
+
+
+// } Driver Code Ends
