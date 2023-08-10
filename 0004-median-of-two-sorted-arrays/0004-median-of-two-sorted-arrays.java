7e970b3f690782c6abb6eb3ffe1e841ce22c792b
dhruveel10 <66111178+dhruveel10@users.noreply.github.com>
2023-08-09 20:39:19 -0400

    Time: 1 ms (100.00%), Space: 44.9 MB (35.03%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,29 +1,53 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int n = n1 + n2;
-        int[] new_arr = new int[n];
-
-        int i=0, j=0, k=0;
-
-        while (i<=n1 && j<=n2) {
-            if (i == n1) {
-                while(j<n2) new_arr[k++] = nums2[j++];
-                break;
-            } else if (j == n2) {
-                while (i<n1) new_arr[k++] = nums1[i++];
-                break;
-            }
-
-            if (nums1[i] < nums2[j]) {
-                new_arr[k++] = nums1[i++];
-            } else {
-                new_arr[k++] = nums2[j++];
+        if(nums1 == null && nums2 == null){
+            return 0;
+        }
+        
+        if(nums1 == null){
+            int n = nums2.length;
+            return nums2[(n - 1) / 2] * 0.5 + nums2[n / 2] * 0.5;
+        }
+        
+        if(nums2 == null){
+            int n = nums1.length;
+            return nums1[(n - 1) / 2] * 0.5 + nums1[n / 2] * 0.5;
+        }
+        
+        if(nums1.length > nums2.length){
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        
+        // choose shorter to binary search
+        int n = nums1.length;
+        int m = nums2.length;
+        int left = 0;
+        int right = n;
+        
+        while(left < right){
+            int i = (left + right) / 2;
+            int j = (n + m) / 2 - i;
+            
+            if(nums1[i] < nums2[j - 1]){
+                left = i + 1;
+            }else{
+                right = i;
             }
         }
-
-        if (n%2==0) return (float)(new_arr[n/2-1] + new_arr[n/2])/2;
-        else return new_arr[n/2];
+        
+        int first = left;
+        int second = (n + m) / 2 - left;
+        
+        int shorterLeft = first == 0 ? Integer.MIN_VALUE : nums1[first - 1];
+        int shorterRight = first == n ? Integer.MAX_VALUE : nums1[first];
+        
+        int longerLeft = second == 0 ? Integer.MIN_VALUE : nums2[second - 1];
+        int longerRight = second == m ? Integer.MAX_VALUE : nums2[second];
+        
+        if((n + m) % 2 == 1) {
+            return Math.min(shorterRight, longerRight);
+        }else{
+            return Math.max(shorterLeft, longerLeft) * 0.5 + Math.min(shorterRight, longerRight) * 0.5;
+        }
     }
 }
