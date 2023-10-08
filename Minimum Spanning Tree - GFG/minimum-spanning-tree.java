27a88302819cf881b77a661374cfdc165249a260
dhruveel10 <66111178+dhruveel10@users.noreply.github.com>
2023-10-08 17:31:29 -0400

    Added solution - LeetHub

---
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.java
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main{
+	static BufferedReader br;
+	static PrintWriter ot;
+    public static void main(String args[]) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		ot = new PrintWriter(System.out);
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-- > 0){
+			String s[] = br.readLine().trim().split(" ");
+			int V = Integer.parseInt(s[0]);
+			int E = Integer.parseInt(s[1]);
+			int edges[][] = new int[E][3];
+			for(int i = 0; i < E; i++){
+				s = br.readLine().trim().split(" ");
+				edges[i][0] = Integer.parseInt(s[0]);
+				edges[i][1] = Integer.parseInt(s[1]);
+				edges[i][2] = Integer.parseInt(s[2]);
+			}
+			ot.println(new Solution().spanningTree(V, E, edges));
+		}
+		ot.close();
+	}
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Pair {
+    int node;
+    int distance;
+    public Pair(int distance, int node) {
+        this.node = node;
+        this.distance = distance;
+    }
+}
+
+class Solution{
+	static int spanningTree(int V, int E, int edges[][]){
+	    // Code Here.
+	    ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<ArrayList<ArrayList<Integer>>>();
+        
+        for(int i = 0; i < V; i++)
+            adj.add(new ArrayList<ArrayList<Integer>>());
+        
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int w = edges[i][2];
+
+            ArrayList<Integer> tmp1 = new ArrayList<Integer>();
+            ArrayList<Integer> tmp2 = new ArrayList<Integer>();
+            tmp1.add(v);
+            tmp1.add(w);
+
+            tmp2.add(u);
+            tmp2.add(w);
+
+            adj.get(u).add(tmp1);
+            adj.get(v).add(tmp2);
+        }
+	    
+	    PriorityQueue<Pair> pq = new PriorityQueue<Pair>((x, y) -> x.distance - y.distance);
+
+        int[] vis = new int[V];
+        // {wt, node}
+        pq.add(new Pair(0, 0));
+        int sum = 0;
+        while (pq.size() > 0) {
+            int wt = pq.peek().distance;
+            int node = pq.peek().node;
+            pq.remove();
+
+            if (vis[node] == 1) continue;
+            // add it to the mst
+            vis[node] = 1;
+            sum += wt;
+
+            for (int i = 0; i < adj.get(node).size(); i++) {
+                int edW = adj.get(node).get(i).get(1);
+                int adjNode = adj.get(node).get(i).get(0);
+                if (vis[adjNode] == 0) {
+                    pq.add(new Pair(edW, adjNode));
+                }
+            }
+        }
+        return sum;
+	}
+}
