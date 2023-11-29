c6983ce23a7f1b8c814227c2d97d6442b0245568
dhruveel10 <66111178+dhruveel10@users.noreply.github.com>
2023-11-29 11:44:10 -0500

    Time: 2 ms (80.05%), Space: 43.2 MB (42.90%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+
+        // Creating a 3D dp array of size [n+1][2][3] initialized to 0
+        int[][][] dp = new int[n + 1][2][k+1];
+
+        // Loop through the dp array, starting from the second last stock (ind=n-1)
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= k; cap++) {
+
+                    if (buy == 0) { // We can buy the stock
+                        dp[ind][buy][cap] = Math.max(0 + dp[ind + 1][0][cap],
+                                -prices[ind] + dp[ind + 1][1][cap]);
+                    }
+
+                    if (buy == 1) { // We can sell the stock
+                        dp[ind][buy][cap] = Math.max(0 + dp[ind + 1][1][cap],
+                                prices[ind] + dp[ind + 1][0][cap - 1]);
+                    }
+                }
+            }
+        }
+
+        // The maximum profit with 2 transactions is stored in dp[0][0][2]
+        return dp[0][0][k];
+    }
+}
